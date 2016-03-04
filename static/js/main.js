a72c06ab902662efcabf0e3b540b9538210d565a
Ricardo Morin <morinricardo@gmail.com>
2016-03-03 17:03:57 -0800

    Major cleanup and styling

---
+++ static/js/main.js
@@ -0,0 +1,28 @@
+/* $(window).scroll(function() {
+    if ($(document).scrollTop() > 150) {
+        $('.navbar').addClass('navbar-shrink');
+    }
+    else {
+        $('.navbar').removeClass('navbar-shrink');
+    }
+}); */
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
